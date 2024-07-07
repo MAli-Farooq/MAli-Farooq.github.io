0268cfc3802fcd912aca25d5dbfb417abef53c90
Muhammad Ali Farooq <mali.frq@gmail.com>
2024-07-07 23:21:08 +0100

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'talks', 'publications', 'projects', 'CV', 'awards']
+const section_names = ['home', 'news', 'talks', 'publications', 'projects', 'CV', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
